cf8a7d219bbd9ebe0dd44cacdf81aed9cc53830f
Kelton Person <kelton.person@gmail.com>
2015-01-15 19:48:13 -0600

    optional demo

--- OptionalStrategy.playground/section-1.swift
+++ OptionalStrategy.playground/section-1.swift
@@ -1,27 +1,36 @@
 // Playground - noun: a place where people can play
 
+//This outlines a strategy for dealing with optional unbinding
+//Will show counter example soon
+
 import UIKit
 
-extension OptionReader {
+protocol Decoding {
+    
+    init?(decoder: Decoder)
+    
+}
 
+extension Decoder {
+    
     var string: String? {
-        return self.value as? String
+        return self.val as? String
     }
     
     var int: Int? {
-        return self.value as? Int
+        return self.val as? Int
     }
     
     var double: Double? {
-        return self.value as? Double
+        return self.val as? Double
     }
     
     var float: Float? {
-        return self.value as? Float
+        return self.val as? Float
     }
     
     var errorMessage: String? {
-        if let err = self.error as NSError? {
+        if let err = self.err as NSError? {
             return err.userInfo!["message"] as String?
         }
         return nil
@@ -29,88 +38,195 @@ extension OptionReader {
     
 }
 
-class OptionReader {
+class Person : Decoding {
+    
+    let name: String
+    var repairs: [String] = []
+    
+    required init?(decoder: Decoder) {
+        self.name = decoder["name"].string!
+        
+        if let r = decoder["car"]["repairs"].arr {
+            self.repairs = map(r, { x in x.string! })
+        }
+        
+    }
+    
+}
+
+struct Decoder {
     
     let notAnArrayError = NSError(domain: "com.optionreader", code: 2, userInfo: [ "message" : "data is not array like" ])
     let notADictionaryError = NSError(domain: "com.optionreader", code: 3, userInfo: [ "message" : "data is not dictionary like" ])
-
-    
-    var dictionary: [String : AnyObject]?
-    var array: [AnyObject]?
-    var value: AnyObject?
-    var error: NSError?
     
     
-    init(dictionary: [String : AnyObject]) {
-        self.dictionary = dictionary
-    }
-    init(array: [AnyObject]) {
-        self.array = array
+    var rawDictionary: [String : AnyObject]?
+    var rawArray: [AnyObject]?
+    private var value: AnyObject?
+    private var error: NSError?
+    let depth: Int
+    
+    init(dictionary: [String : AnyObject], depth: Int = 0) {
+        self.rawDictionary = dictionary
+        self.depth = depth
     }
     
-    init(value: AnyObject) {
-        self.value = value
+    init(array: [AnyObject], depth: Int = 0) {
+        self.rawArray = array
+        self.depth = depth
     }
     
-    init(error: NSError) {
+    init(value: AnyObject, parseType: Bool = false, depth: Int = 0) {
+        self.depth = depth
+        if !parseType {
+            self.value = value
+        }
+        else {
+            if let a = value as? [AnyObject] {
+                self.rawArray = a
+            }
+            else if let d = val as? [String : AnyObject] {
+                self.rawDictionary = d
+            }
+            else {
+                self.value = value
+            }
+        }
+    }
+    
+    private init(error: NSError, depth: Int = 0) {
+        self.depth = depth
         self.error = error
     }
     
-    func indexOutRangeError(index: Int) -> NSError {
-        return NSError(domain: "com.optionreader", code: 1, userInfo: [ "message" : "index \(index) of range" ])
-    }
-    
-    func keyNotPresentError(key: String) -> NSError {
-        return NSError(domain: "com.optionreader", code: 1, userInfo: [ "message" : "key \(key) not present" ])
-    }
-    
-    subscript(index: Int) -> OptionReader {
-        if let e = error {
-            return OptionReader(error: e)
+    var arr: DecoderArray? {
+        if let a = rawArray {
+            return DecoderArray(items: a)
         }
         else {
-            if let a = array {
+            return nil
+        }
+    }
+    
+    var dict: DecoderDictionary? {
+        if let a = rawDictionary {
+            return DecoderDictionary(items: a)
+        }
+        else {
+            return nil
+        }
+    }
+    
+    var val: AnyObject? {
+        return self.value
+    }
+    
+    var err: NSError? {
+        return self.error
+    }
+    
+    
+    private func indexOutRangeError(index: Int) -> NSError {
+        return NSError(domain: "com.optionreader", code: 1, userInfo: [ "message" : "index \(index) of range of array at depth \(self.depth)" ])
+    }
+    
+    private func keyNotPresentError(key: String) -> NSError {
+        return NSError(domain: "com.optionreader", code: 1, userInfo: [ "message" : "key '\(key)' not present in dictionary at depth \(self.depth)" ])
+    }
+    
+    subscript(index: Int) -> Decoder {
+        if let e = error {
+            return Decoder(error: e)
+        }
+        else {
+            if let a = rawArray {
                 if index >= a.count {
-                    return OptionReader(error: indexOutRangeError(index))
+                    return Decoder(error: indexOutRangeError(index), depth : depth + 1)
                 }
                 else {
                     return mapToData(a[index])
                 }
             }
             else {
-                return OptionReader(error: notAnArrayError)
+                return Decoder(error: notAnArrayError)
             }
         }
     }
     
-    subscript(key: String) -> OptionReader {
-        if let d = dictionary {
+    subscript(key: String) -> Decoder {
+        if let d = rawDictionary {
             if let val: AnyObject = d[key] {
                 return mapToData(val)
             }
             else {
-                return OptionReader(error: keyNotPresentError(key))
+                return Decoder(error: keyNotPresentError(key), depth : depth + 1)
             }
         }
         else {
-            return OptionReader(error: notADictionaryError)
+            return Decoder(error: keyNotPresentError(key), depth : depth + 1)
         }
     }
     
-    func mapToData(val: AnyObject) -> OptionReader {
-        if let arr = val as? [AnyObject] {
-            return OptionReader(array: arr)
+    func mapToData(val: AnyObject) -> Decoder {
+        if let a = val as? [AnyObject] {
+            return Decoder(array: a, depth : depth + 1)
         }
-        else if let dict = val as? [String : AnyObject] {
-            return OptionReader(dictionary: dict)
+        else if let d = val as? [String : AnyObject] {
+            return Decoder(dictionary: d, depth : depth + 1)
         }
         else {
-            return OptionReader(value: val)
+            return Decoder(value: val, depth : depth + 1)
         }
     }
     
 }
 
+struct DecoderArray : SequenceType {
+    
+    let items: [AnyObject]
+    
+    func generate() -> GeneratorOf<Decoder> {
+        
+        var i = -1
+        
+        return GeneratorOf<Decoder> {
+            i++
+            if( i < self.items.count) {
+                return Decoder(value: self.items[i], parseType: true)
+            }
+            else {
+                return nil
+            }
+        }
+    }
+    
+    
+}
+
+struct DecoderDictionary : SequenceType {
+    
+    let items: [String: AnyObject]
+    
+    func generate() -> GeneratorOf<(String, Decoder)> {
+        
+        var i = -1
+        let keys = items.keys.array
+        
+        return GeneratorOf<(String, Decoder)> {
+            i++
+            if( i < self.items.count) {
+                let key = keys[i]
+                let value =  Decoder(value: self.items[key]!, parseType: true)
+                return (key, value)
+            }
+            else {
+                return nil
+            }
+        }
+    }
+    
+    
+}
 
 let bob: [String:AnyObject] =
 [
@@ -120,33 +236,26 @@ let bob: [String:AnyObject] =
         [ "name" : "Eric", "age" : 11 ],
         [ "name" : "Chelsey", "age" : 10 ]
     ],
-    "car" : "Honda"
+    "car" : [
+        "name" : "Honda",
+        "repairs" : ["oil", "brakes"]
+    ]
 ]
 
 let susie: [String:AnyObject] =
 [
     "name" : "Susie",
-    "scheduleDays" : [2, 4, 5, 4],
+    "scheduleDays" : [2, 3, 6, 4],
     "friends" : [
-        [ "name" : "Eric", "age" : 11 ],
-        [ "name" : "Chelsey", "age" : 10 ]
+        [ "name" : "Eric", "age" : 10 ],
+        [ "name" : "Chelsey", "age" : 11 ]
     ]
 ]
 
-let dataDict = OptionReader(dictionary: bob)
-let dataArr = OptionReader(array: [bob, susie])
+let bobDict = Decoder(dictionary: bob)
+let susieDict = Decoder(dictionary: susie)
 
+let dataArr = Decoder(array: [bob, susie])
 
-dataArr[0]["car"].string
-
-dataArr[0]["name"].string
-dataArr[1]["name"].string
-
-dataArr[2]["name"].errorMessage
-
-dataDict["name"].string
-
-let arr = dataArr.array!
-
-
-
+let bobPerson = Person(decoder: bobDict)
+let susiePerson = Person(decoder: susieDict)
