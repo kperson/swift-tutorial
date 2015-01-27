51f975bf8251c17bb171987b7d51959b21057b02
Eric Figura <efigura@solstice-consulting.com>
2015-01-27 11:56:38 -0600

    Updating function comments

--- Functions.playground/section-1.swift
+++ Functions.playground/section-1.swift
@@ -4,24 +4,29 @@ import UIKit
 
 //*************************************
 
-//func sayHello() {
-//    println("hello")
-//}
-//
-//func sayGoodbye() {
-//    println("goodbye")
-//}
-//
-//func speak(speakFunction: ()) {
-//    speakFunction
-//}
-//
-//speak(sayHello())
-//speak(sayGoodbye())
+func sayHello() -> Void {
+    println("hello")
+}
 
+func sayGoodbye() -> Void {
+    println("goodbye")
+}
+
+func speak(speakFunction: () ->Void) {
+    speakFunction()
+}
+
+speak(sayHello)
+speak(sayGoodbye)
+
+let y: () -> Void = {
+    println("hi")
+}
+
+speak(y)
 
 //*************************************
-
+//Old way
 let numbersArray = [2,4,6,8]
 
 var mapArray:[String] = []
@@ -29,25 +34,26 @@ var mapArray:[String] = []
 for money  in numbersArray {
     mapArray.append("$\(money).00")
 }
-
 println(mapArray)
 
+//New way
 mapArray = numbersArray.map({ "$\($0).00" })
 println(mapArray)
 
 //**************************************
-
+//Old way
 var sum = 0
 for money  in numbersArray {
     sum += money
 }
 println(sum)
 
+//New Way
 sum = numbersArray.reduce(0, {$0 + $1})
 println(sum)
 
 //**************************************
-
+//Old way
 var oldFilteredNumbers:[Int] = []
 
 for money  in numbersArray {
@@ -55,15 +61,15 @@ for money  in numbersArray {
         oldFilteredNumbers.append(money)
     }
 }
-
 println(oldFilteredNumbers)
 
+//New way
 let filteredNumbers = numbersArray.filter({$0 % 3 == 0})
 
 println(filteredNumbers)
 
 //**************************************
-
+//Chaining
 let filterAndMappedValues = numbersArray.filter({$0 % 3 == 0}).map({ "$\($0).00" })
 print(filterAndMappedValues)
 
